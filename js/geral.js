eb84fc0accd1b6fa8b46117244385c21d65ec21e
BrunoDahlem <53070061+BrunoDahlem@users.noreply.github.com>
2019-11-21 17:36:00 -0300

    Add files via upload

--- js/geral.js
+++ js/geral.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
-    $('.burger').on('click', function () {
-        $('.burger').toggleClass('collapsed');
-        $('.burger').attr('aria-expanded','true');
+    $('.burguer').click (function () {
         $('.animated-icon3').toggleClass('open');
+        $('.x').toggleClass('menuMobile');
     });
-  });
+});
